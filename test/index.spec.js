10f09186e3a7a9c943ae44c40c5439d867349913
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-04-16 11:09:54 +0700

    test for authentication for admin

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,22 +2,30 @@
 
 let supertest = require('supertest');
 let staticNow = require('static-now');
+
 let app = require("../");
-let config = require('../config');
+let config = require('../config')();
+
+let request = supertest.agent(app.listen());
 
 describe('Mounting of parts', function () {
     it('the site shows up nicely', function (done) {
-        let request = supertest.agent(app.listen());
         request
             .get('/index.html')
             .expect(200)
             .end(done);
     });
-   //  it('the admin part shows up, when you log in', function (done) {
-   //  	request
-			// .get('/admin')
-			// .auth(config.adminUser.name, testHelpers.adminUser.pass)
-			// .expect(200)
-			// .end(done);
-   //  });
+    it('the admin part shows up, when you log in', function (done) {
+    	request
+			.get('/admin')
+			.auth(config.adminUser.name, config.adminUser.pass)
+			.expect(200)
+			.end(done);
+    });
+    it('the admin part doesnt shows up, when you not log in', function (done) {
+    	request
+			.get('/admin')
+			.expect(401)
+			.end(done);
+    });
 });
