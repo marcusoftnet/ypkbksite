08b6128c8013c5fe4f723d24eeba02c60bb1101b
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-08-11 15:57:10 +0700

    Refactored the tests for modal windows

--- site/test/modalWindows.spec.js
+++ site/test/modalWindows.spec.js
@@ -11,8 +11,12 @@ let testHelpers = require('../../test/testHelpers.js');
 
 let request = supertest.agent(app.listen());
 
-describe('The modal windows', function () {
-	beforeEach(function (done) {
+describe('The site has modal windows ', function () {
+	let today = new Date();
+    let tomorrow = new Date();
+    let yesterday = new Date();
+
+    beforeEach(function (done) {
 		testHelpers.removeAllDocs(done);
 	});
 
@@ -20,36 +24,31 @@ describe('The modal windows', function () {
 		testHelpers.removeAllDocs(done);
 	});
 
-	describe("Articles", function () {
-		let today = new Date();
-        let tomorrow = new Date();
-        let yesterday = new Date();
+    it('for articles', function  (done) {
+        co(function *() {
 
-        beforeEach(function (done) {
-            today.setHours(0,0,0,0);
+        	today.setHours(0,0,0,0);
             tomorrow.setDate(today.getDate()+1);
             yesterday.setDate(today.getDate()- 1);
             tomorrow.setHours(0,0,0,0);
             yesterday.setHours(0,0,0,0);
-            done();
-        });
 
-        it('are retrieved by slug', function  (done) {
-            co(function *() {
-                yield db.articlesCollection.insert({
-                	title: "Article 1", 
-                	slug: "article-1", 
-                	publishStart : today, 
-                	publishEnd : tomorrow 
-                });
-
-                request
-                    .get('/article/article-1')
-                    .expect(function (res) {
-                        res.text.should.containEql("Article 1");
-                    })
-                    .end(done);
+            yield db.articlesCollection.insert({
+            	title: "Article 1", 
+            	slug: "article-1", 
+            	publishStart : today, 
+            	publishEnd : tomorrow 
             });
+
+            request
+                .get('/article/article-1')
+                .expect(function (res) {
+                    res.text.should.containEql("Article 1");
+                })
+                .end(done);
         });
 	});
+
+	it('for hospitals');
+	it('for clinics');
 });
